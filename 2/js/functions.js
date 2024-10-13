eb7f85068ff0238702d6b1d2b7aa8fb32a4beeb2
Keks <keks@htmlacademy.ru>
2024-10-13 18:09:23 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const numbersLine = 20;
+// eslint-disable-next-line no-unused-vars, no-shadow
+const lineLength = function (line, numbersLine) {
+  if (line <= 20) {
+    // eslint-disable-next-line no-shadow
+    const result = line.length;
+    console.log (result,'true');
+  }
+  const result = line.length;
+  // eslint-disable-next-line no-console
+  console.log (result,'false');
+};
+
+// eslint-disable-next-line no-unused-vars
+const palindrom = function (wordPalindrom);
+// eslint-disable-next-line no-undef
+console.log(toLowerCase(palindrom));
