9717648017813d2198a5ecd2665b2b9a2704ae76
Арина <sokolova.1978@icloud.com>
2024-10-14 14:56:33 +0300

    Написали работающие функции

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,22 @@
-function checkStringLength (string, number) {
-return string.length <= number ;
+function checkStringLength(string, number) {
+  return string.length <= number;
 }
 
 console.log("октябрь 20 = " + checkStringLength('октябрь', 20));
 console.log("октябрь 2 = " + checkStringLength('октябрь', 2));
 
-function isPalindrome (string) {
-string = string.replaceAll(' ', '');
-string = string.toUpperCase();
-let result= '';
-return
+function isPalindrome(string) {
+  let newString = string.split('').reverse().join('');
+  let result;
+  if (newString === string) {
+    result = 'Да, это палиндром';
+  }
+  else {
+    result = 'Нет, не палиндром';
+  }
+  return result;
 }
 
-console.log('дОвОд = ' + 'Да, палиндром' );
-console.log('шишка = ' + 'Нет, не палиндром');
+console.log("топот = " + isPalindrome('топот'));
+console.log("шишка = " + isPalindrome('шишка'));
+
